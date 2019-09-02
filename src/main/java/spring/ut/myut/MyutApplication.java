3fdb180b56ed6fdfc61ab3c9ed37f19ec0780f2d
venkatasandu <venkata.sandu@gmail.com>
2019-09-02 21:43:10 +0530

    for amazon ec2 jenkins

--- src/main/java/spring/ut/myut/MyutApplication.java
+++ src/main/java/spring/ut/myut/MyutApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyutApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+		
 		SpringApplication.run(MyutApplication.class, args);
 	}
 
